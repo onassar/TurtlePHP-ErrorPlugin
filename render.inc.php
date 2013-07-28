90e83d2fc5042ee4dab3d56acd6b5379857182b1
Oliver Nassar <onassar@gmail.com>
2013-07-28 17:42:38 -0400

    Trying to update includes to work with new bootstrap

--- render.inc.php
+++ render.inc.php
@@ -4,8 +4,11 @@
         <meta charset="utf-8" />
         <title>Error</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" type="text/css" href="http://twitter.github.io/bootstrap/assets/css/bootstrap.css" />
-        <link rel="stylesheet" type="text/css" href="http://twitter.github.io/bootstrap/assets/js/google-code-prettify/prettify.css" />
+        <!-- <link rel="stylesheet" type="text/css" href="http://twitter.github.io/bootstrap/assets/css/bootstrap.css" /> -->
+        <!-- <link rel="stylesheet" type="text/css" href="http://twitter.github.io/bootstrap/dist/css/bootstrap.css" /> -->
+        <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" />
+        <!-- <link rel="stylesheet" type="text/css" href="http://twitter.github.io/bootstrap/assets/js/google-code-prettify/prettify.css" /> -->
+        <link rel="stylesheet" type="text/css" href="http://google-code-prettify.googlecode.com/svn/trunk/src/prettify.css" />
         <script type="text/javascript">
         //<![CDATA[
             var start=(new Date).getTime(),booted=[],included=false,required=[],js=function(e,t){if(arguments.length===0){t=function(){};e=[]}else if(arguments.length===1){t=e;e=[]}var n=function(e,t){var n=document.createElement("script"),r=document.getElementsByTagName("script"),s=r.length,o=function(){try{t&&t()}catch(e){i(e)}};n.setAttribute("type","text/javascript");n.setAttribute("charset","utf-8");if(n.readyState){n.onreadystatechange=function(){if(n.readyState==="loaded"||n.readyState==="complete"){n.onreadystatechange=null;o()}}}else{n.onload=o}n.setAttribute("src",e);document.body.insertBefore(n,r[s-1].nextSibling)},r=function(e,t){for(var n=0,r=e.length;n<r;++n){if(e[n]===t){return true}}return false},i=function(e){log("Caught Exception:");log(e.stack);log("")};if(included===false){if(typeof e==="string"){e=[e]}e=e.concat(required);included=true}if(typeof e==="string"){if(r(booted,e)){t()}else{booted.push(e);n(e,t)}}else if(e.constructor===Array){if(e.length!==0){js(e.shift(),function(){js(e,t)})}else{try{t&&t()}catch(s){i(s)}}}},log=function(){if(typeof console!=="undefined"&&console&&console.log){var e=arguments.length>1?arguments:arguments[0];console.log(e)}},queue=function(){var e=[];return{push:function(t){e.push(t)},process:function(){var t;while(t=e.shift()){t()}}}}(),ready=function(e){var t=false,n=true,r=window.document,i=r.documentElement,s=r.addEventListener?"addEventListener":"attachEvent",o=r.addEventListener?"removeEventListener":"detachEvent",u=r.addEventListener?"":"on",a=function(n){if(n.type==="readystatechange"&&r.readyState!=="complete"){return}(n.type==="load"?window:r)[o](u+n.type,a,false);if(!t&&(t=true)){e()}},f=function(){try{i.doScroll("left")}catch(e){setTimeout(f,50);return}a("poll")};if(r.readyState==="complete"){e.call(window,"lazy")}else{if(r.createEventObject&&i.doScroll){try{n=!window.frameElement}catch(l){}if(n){f()}}r[s](u+"DOMContentLoaded",a,false);r[s](u+"readystatechange",a,false);window[s](u+"load",a,false)}},require=function(e){if(typeof e==="string"){e=[e]}required=required.concat(e)}
@@ -109,7 +112,7 @@
                 padding: 0 0 0 24px;
             }
         </style>
-        <link rel="stylesheet" type="text/css" href="http://twitter.github.io/bootstrap/assets/css/bootstrap-responsive.css" />
+        <!-- <link rel="stylesheet" type="text/css" href="http://twitter.github.io/bootstrap/assets/css/bootstrap-responsive.css" /> -->
     </head>
     <body>
         <div class="container">
@@ -190,7 +193,8 @@
                 log('ready.post', (new Date()).getTime() - start);
                 js(
                     [
-                        'http://twitter.github.io/bootstrap/assets/js/google-code-prettify/prettify.js',
+                        // 'http://twitter.github.io/bootstrap/assets/js/google-code-prettify/prettify.js',
+                        'https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js',
                         'https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js'
                     ],
                     function() {
