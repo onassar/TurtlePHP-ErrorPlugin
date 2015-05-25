300c0bdf215e9a29ef6edf35feb649701a7cc244
Oliver Nassar <onassar@gmail.com>
2015-05-25 17:51:30 -0400

    Status codes for proper error handling

--- render.inc.php
+++ render.inc.php
@@ -1,3 +1,14 @@
+<?php
+
+    // error headers
+    header(
+        ($_SERVER['SERVER_PROTOCOL']) . ' 503 Service Temporarily Unavailable'
+    );
+    header('Status: 503 Service Temporarily Unavailable');
+    header('Retry-After: 7200');
+    header('Content-Type: text/html; charset=utf-8');
+
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
