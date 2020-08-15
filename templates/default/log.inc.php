b0eaf338aa1c2a4f5ba7b2c1fb36cf78d4fa8434
onassar <onassar@gmail.com>
2020-08-15 17:03:41 -0400

    Referrer

--- templates/default/log.inc.php
+++ templates/default/log.inc.php
@@ -1,5 +1,6 @@
-Message:   <?= $throwable->getMessage() . (PHP_EOL) ?>
-File:      <?= $throwable->getFile() . (PHP_EOL) ?>
-Line:      <?= $throwable->getLine() . (PHP_EOL) ?>
-IP:        <?= (IP) . (PHP_EOL) ?>
-URI:       <?= ($_SERVER['REQUEST_URI'] ?? $_SERVER['SCRIPT_NAME'] ?? null) . (PHP_EOL) ?>
+Message:        <?= $throwable->getMessage() . (PHP_EOL) ?>
+File:           <?= $throwable->getFile() . (PHP_EOL) ?>
+Line:           <?= $throwable->getLine() . (PHP_EOL) ?>
+IP:             <?= (IP) . (PHP_EOL) ?>
+URI:            <?= ($_SERVER['REQUEST_URI'] ?? $_SERVER['SCRIPT_NAME'] ?? null) . (PHP_EOL) ?>
+Referrer:       <?= ($_SERVER['HTTP_REFERER'] ?? ':unknown:') . (PHP_EOL) ?>
