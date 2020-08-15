9bfb7c36c76459c0a033f67650ddf589ab74f07d
onassar <onassar@gmail.com>
2020-08-15 17:53:46 -0400

    Better logging

--- templates/default/log.inc.php
+++ templates/default/log.inc.php
@@ -1,6 +1,7 @@
-Message:        <?= $throwable->getMessage() . (PHP_EOL) ?>
-File:           <?= $throwable->getFile() . (PHP_EOL) ?>
-Line:           <?= $throwable->getLine() . (PHP_EOL) ?>
-IP:             <?= (IP) . (PHP_EOL) ?>
-URI:            <?= ($_SERVER['REQUEST_URI'] ?? $_SERVER['SCRIPT_NAME'] ?? null) . (PHP_EOL) ?>
-Referrer:       <?= ($_SERVER['HTTP_REFERER'] ?? ':unknown:') . (PHP_EOL) ?>
+Message:            <?= $throwable->getMessage() . (PHP_EOL) ?>
+File:               <?= $throwable->getFile() . (PHP_EOL) ?>
+Line:               <?= $throwable->getLine() . (PHP_EOL) ?>
+IP:                 <?= (IP) . (PHP_EOL) ?>
+URI:                <?= ($_SERVER['REQUEST_URI'] ?? $_SERVER['SCRIPT_NAME'] ?? null) . (PHP_EOL) ?>
+Referrer:           <?= ($_SERVER['HTTP_REFERER'] ?? ':unknown:') . (PHP_EOL) ?>
+Request Method:     <?= ($_SERVER['REQUEST_METHOD'] ?? ':unknown:') . (PHP_EOL) ?>
