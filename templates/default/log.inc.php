3faae5446d0bdd36aee1f8c8435d1fac26f98894
onassar <onassar@gmail.com>
2020-08-16 01:32:28 -0400

    logging

--- templates/default/log.inc.php
+++ templates/default/log.inc.php
@@ -1,8 +1,10 @@
+
+*Error*
 Message:            <?= $throwable->getMessage() . (PHP_EOL) ?>
 File:               <?= $throwable->getFile() . (PHP_EOL) ?>
 Line:               <?= $throwable->getLine() . (PHP_EOL) ?>
 IP:                 <?= (IP) . (PHP_EOL) ?>
-URI:                <?= ($_SERVER['REQUEST_URI'] ?? $_SERVER['SCRIPT_NAME'] ?? null) . (PHP_EOL) ?>
-Referrer:           <?= ($_SERVER['HTTP_REFERER'] ?? ':unknown:') . (PHP_EOL) ?>
+Request URI:        <?= ($_SERVER['REQUEST_URI'] ?? $_SERVER['SCRIPT_NAME'] ?? null) . (PHP_EOL) ?>
+HTTP Referrer:      <?= ($_SERVER['HTTP_REFERER'] ?? ':unknown:') . (PHP_EOL) ?>
 Request Method:     <?= ($_SERVER['REQUEST_METHOD'] ?? ':unknown:') . (PHP_EOL) ?>
-Agent:              <?= ($_SERVER['HTTP_USER_AGENT'] ?? ':unknown:') . (PHP_EOL) ?>
+User Agent:         <?= ($_SERVER['HTTP_USER_AGENT'] ?? ':unknown:') . (PHP_EOL) ?>
