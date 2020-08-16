09694ef560034cf8baf9fdda9c9f417583332bbf
onassar <onassar@gmail.com>
2020-08-15 21:32:54 -0400

    User agent tracking for errors

--- templates/default/log.inc.php
+++ templates/default/log.inc.php
@@ -5,3 +5,4 @@ IP:                 <?= (IP) . (PHP_EOL) ?>
 URI:                <?= ($_SERVER['REQUEST_URI'] ?? $_SERVER['SCRIPT_NAME'] ?? null) . (PHP_EOL) ?>
 Referrer:           <?= ($_SERVER['HTTP_REFERER'] ?? ':unknown:') . (PHP_EOL) ?>
 Request Method:     <?= ($_SERVER['REQUEST_METHOD'] ?? ':unknown:') . (PHP_EOL) ?>
+Agent:              <?= ($_SERVER['HTTP_USER_AGENT'] ?? ':unknown:') . (PHP_EOL) ?>
