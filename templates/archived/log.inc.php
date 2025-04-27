b451f8b5111ddbc2f42274010db03c5262c9d635
Oliver Nassar <onassar@Mac.ht.home>
2025-04-27 18:24:09 -0400

    [AUTOMATED] Test add commit & push

--- templates/archived/log.inc.php
+++ templates/archived/log.inc.php
@@ -4,6 +4,7 @@ Message:            <?= $throwable->getMessage() . (PHP_EOL) ?>
 File:               <?= $throwable->getFile() . (PHP_EOL) ?>
 Line:               <?= $throwable->getLine() . (PHP_EOL) ?>
 IP:                 <?= (IP) . (PHP_EOL) ?>
+Server Name:        <?= ($_SERVER['SERVER_NAME'] ?? null) . (PHP_EOL) ?>
 Request URI:        <?= ($_SERVER['REQUEST_URI'] ?? $_SERVER['SCRIPT_NAME'] ?? null) . (PHP_EOL) ?>
 HTTP Referrer:      <?= ($_SERVER['HTTP_REFERER'] ?? ':unknown:') . (PHP_EOL) ?>
 Request Method:     <?= ($_SERVER['REQUEST_METHOD'] ?? ':unknown:') . (PHP_EOL) ?>
